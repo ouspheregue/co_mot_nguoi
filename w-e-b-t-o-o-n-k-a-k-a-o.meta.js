5c5d7a1061aefa80cef1c819b2e7630eff5e3219
ouspheregue <ouspheregue@github.com>
2024-08-16 16:34:02 +0700

    up

--- w-e-b-t-o-o-n-k-a-k-a-o.meta.js
+++ w-e-b-t-o-o-n-k-a-k-a-o.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         w-e-b-t-o-o-n-k-a-k-a-o
 // @namespace    https://github.com/ouspheregue/co_mot_nguoi/raw/main/w-e-b-t-o-o-n-k-a-k-a-o.user.js
-// @version      2024-08-16
+// @version      2024.08.16.1
 // @description  down w-e-b-t-o-o-n-k-a-k-a-o
 // @author       You
-// @include      /^https:\/\/w[e3]bt[o0][o0]n.k[a@]ka[o0].c[o0]m/vi[e3]wer\//
+// @include      /^https:\/\/w[e3]bt[o0][o0]n\.k[a@]ka[o0]\.c[o0]m\/vi[e3]wer\//
 // @icon         https://raw.githubusercontent.com/ouspheregue/co_mot_nguoi/main/w-e-b-t-o-o-n-k-a-k-a-o.png
 // @grant        GM_download
 // @grant        GM_info
