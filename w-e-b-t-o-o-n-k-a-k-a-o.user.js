f873a83b12ea330cd7c5bd1c5ac3024279e31cd2
ouspheregue <ouspheregue@github.com>
2025-04-16 09:58:08 +0700

    up

--- w-e-b-t-o-o-n-k-a-k-a-o.user.js
+++ w-e-b-t-o-o-n-k-a-k-a-o.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         w-e-b-t-o-o-n-k-a-k-a-o
-// @namespace    https://github.com/ouspheregue/co_mot_nguoi/raw/main/w-e-b-t-o-o-n-k-a-k-a-o.user.js
-// @version      2025.04.16
+// @namespace    w-e-b-t-o-o-n-k-a-k-a-o.user.js
+// @version      2025.04.16.1
 // @description  down w-e-b-t-o-o-n-k-a-k-a-o
 // @author       You
 // @include      /^https:\/\/w[e3]bt[o0][o0]n\.k[a@]ka[o0]\.c[o0]m\/vi[e3]wer\//
