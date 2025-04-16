5d3fd478a9cb86383686b26892f8b7fb9dc9e2cb
ouspheregue <ouspheregue@github.com>
2025-04-16 09:48:13 +0700

    up

--- w-e-b-t-o-o-n-k-a-k-a-o.meta.js
+++ w-e-b-t-o-o-n-k-a-k-a-o.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         w-e-b-t-o-o-n-k-a-k-a-o
 // @namespace    https://github.com/ouspheregue/co_mot_nguoi/raw/main/w-e-b-t-o-o-n-k-a-k-a-o.user.js
-// @version      2024.08.16.1
+// @version      2025.04.16
 // @description  down w-e-b-t-o-o-n-k-a-k-a-o
 // @author       You
 // @include      /^https:\/\/w[e3]bt[o0][o0]n\.k[a@]ka[o0]\.c[o0]m\/vi[e3]wer\//
 // @icon         https://raw.githubusercontent.com/ouspheregue/co_mot_nguoi/main/w-e-b-t-o-o-n-k-a-k-a-o.png
-// @grant        GM_download
-// @grant        GM_info
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
 // @downloadURL  https://github.com/ouspheregue/co_mot_nguoi/raw/main/w-e-b-t-o-o-n-k-a-k-a-o.user.js
 // @updateURL    https://github.com/ouspheregue/co_mot_nguoi/raw/main/w-e-b-t-o-o-n-k-a-k-a-o.meta.js
 // ==/UserScript==
